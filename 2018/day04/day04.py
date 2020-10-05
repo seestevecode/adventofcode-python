4127dac5d239d2d2aed2633bc2784fe379915469
Steve Hill <stevhil+github@gmail.com>
2020-10-05 20:26:13 +0100

    Remove test lines from day 4

--- 2018/day04/day04.py
+++ 2018/day04/day04.py
@@ -5,28 +5,6 @@ with open(input_path) as input_list:
     formatted_list = [line.rstrip() for line in input_list]
 formatted_list.sort()
 
-"""
-formatted_list = [
-    "[1518-11-01 00:00] Guard #10 begins shift",
-    "[1518-11-01 00:05] falls asleep",
-    "[1518-11-01 00:25] wakes up",
-    "[1518-11-01 00:30] falls asleep",
-    "[1518-11-01 00:55] wakes up",
-    "[1518-11-01 23:58] Guard #99 begins shift",
-    "[1518-11-02 00:40] falls asleep",
-    "[1518-11-02 00:50] wakes up",
-    "[1518-11-03 00:05] Guard #10 begins shift",
-    "[1518-11-03 00:24] falls asleep",
-    "[1518-11-03 00:29] wakes up",
-    "[1518-11-04 00:02] Guard #99 begins shift",
-    "[1518-11-04 00:36] falls asleep",
-    "[1518-11-04 00:46] wakes up",
-    "[1518-11-05 00:03] Guard #99 begins shift",
-    "[1518-11-05 00:45] falls asleep",
-    "[1518-11-05 00:55] wakes up"
-]
-"""
-
 guard_sleep_time = {}
 guard_minutes_asleep = {}
 for line in formatted_list:
