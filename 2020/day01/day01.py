6f664af2217a8c195bc99a0e094ead25e72d12e5
Steve Hill <stevhil+github@gmail.com>
2020-12-02 20:49:35 +0000

    Complete day 1

---
+++ 2020/day01/day01.py
@@ -0,0 +1,15 @@
+import os
+input_path = os.path.join(os.path.dirname(__file__), 'input')
+with open(input_path) as input_list:
+    formatted_list = [int(line) for line in input_list]
+
+part_one = [i for i in formatted_list if 2020 - i in formatted_list]
+print (part_one[0] * part_one[1])
+
+for i in formatted_list:
+    part_two = [j for j in formatted_list if 2020 - i - j in formatted_list]
+    if part_two != []:
+        print (i * part_two[0] * part_two[1])
+        break
+
+
